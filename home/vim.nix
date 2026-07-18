bf435b76f6e12bb4b08fdf4b56bc43dd477f4b08
zizimiku <zizimiku@outlook.com>
2026-07-18 18:27:16 +0800

    refactor: consolidate pure packages, split only where config logic justifies it

--- home/vim.nix
+++ home/vim.nix
@@ -6,8 +6,8 @@
       set number
       set relativenumber
       set expandtab
-      set shiftwidth=2
-      set tabstop=2
+      set shiftwidth=4
+      set tabstop=4
       set smartindent
     '';
   };
